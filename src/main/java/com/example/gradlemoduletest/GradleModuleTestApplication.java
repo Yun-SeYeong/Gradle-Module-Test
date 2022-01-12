e08f93cfa6166626e97d25c5bf00014cc328431e
Yun-SeYeong <dbstpdud09@naver.com>
2022-01-12 22:38:53 +0900

    [ADD] init spring project

---
+++ src/main/java/com/example/gradlemoduletest/GradleModuleTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gradlemoduletest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleModuleTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GradleModuleTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gradlemoduletest/GradleModuleTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradlemoduletest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleModuleTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
